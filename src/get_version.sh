030e712f65c16530786c0a842bcbd6ed5302b8e8
grenaud <gabriel.reno@gmail.com>
2023-02-06 22:23:09 +0100

    fixed version

--- src/get_version.sh
+++ src/get_version.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
 version=$(git describe --tags $(git rev-list --tags --max-count=1))
-echo "#define VERSION \"$version\"" > version.h
+
+if [ $? -eq 0 ]; then
+    echo "#define VERSION \"$version\"" > version.h
+else
+    echo "#define VERSION "NA"" > version.h
+fi
