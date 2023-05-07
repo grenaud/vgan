29ba38cb7cab96d75f1add15027a2132ca516e18
Nicola Alexandra Vogel <61189065+nicolaavogel@users.noreply.github.com>
2023-05-07 14:18:50 +0200

    Update visualize_detected_taxa.sh

--- bin/visualize_detected_taxa.sh
+++ bin/visualize_detected_taxa.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+PATHTOFILE=$(dirname $(readlink -f "$0"))
+
 less ${1}_detected.tsv | cut -f1 | tail -n +2 > names.txt
 
 for val in $(cat names.txt)
 do
-	Rscript plot_taxon.R $1 $val
+        Rscript ${PATHTOFILE}/plot_taxon.R $1 $val
 
 done
 
